ccd7fd23d70d496bcf18934049352c835bc0b432
edgar-walugembe <edgar.walugembe.upti@gmail.com>
2024-10-05 19:12:55 +0300

    city: calgary completed

--- calgary/autotrader.js
+++ calgary/autotrader.js
@@ -1,7 +1,130 @@
 const playwright = require("playwright");
 const { v4: uuidv4 } = require("uuid");
-const dotenv = require("dotenv");
-dotenv.config();
+const axios = require("axios");
+
+function getMainBodyType(bodyType) {
+  const bodyTypes = {
+    hatchback: [
+      "3-door hatchback",
+      "5-door hatchback",
+      "compact hatchback",
+      "subcompact hatchback",
+    ],
+    suv: [
+      "suv",
+      "crossover",
+      "compact suv",
+      "midsize suv",
+      "full-size suv",
+      "off-road suv",
+      "luxury suv",
+      "subcompact suv",
+    ],
+    coupe: [
+      "coupe",
+      "sports-utility",
+      "sports coupe",
+      "grand tourer",
+      "hardtop coupe",
+      "luxury coupe",
+      "performance coupe",
+    ],
+    truck: [
+      "truck",
+      "crew cab",
+      "pickup truck",
+      "pickup",
+      "light duty truck",
+      "heavy duty truck",
+      "extended cab",
+      "extended cab truck",
+      "compact truck",
+      "off-road truck",
+      "full-size truck",
+    ],
+    sedan: [
+      "compact sedan",
+      "subcompact sedan",
+      "midsize sedan",
+      "full-size sedan",
+      "executive sedan",
+      "luxury sedan",
+      "sports sedan",
+    ],
+    wagon: [
+      "wagon",
+      "super-wagon",
+      "station wagon",
+      "estate wagon",
+      "sports wagon",
+      "crossover wagon",
+      "luxury wagon",
+    ],
+    convertible: [
+      "convertible",
+      "roadster",
+      "cabriolet",
+      "hardtop convertible",
+      "soft-top convertible",
+      "targa",
+      "2-seater convertible",
+    ],
+    van: [
+      "van",
+      "minivan",
+      "passenger van",
+      "cargo van",
+      "full-size van",
+      "compact van",
+      "conversion van",
+      "camper van",
+    ],
+  };
+
+  try {
+    bodyType = bodyType.toLowerCase().replace(/[-\s]+/g, " ");
+
+    for (let mainType in bodyTypes) {
+      if (mainType === bodyType || bodyTypes[mainType].includes(bodyType)) {
+        return mainType;
+      }
+    }
+
+    throw new Error("Body type not found");
+  } catch (error) {
+    return "Not Available";
+  }
+}
+
+const cars = [];
+
+// function to split cars array into smaller chunks
+function chunkArray(array, size) {
+  const result = [];
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+  return result;
+}
+
+async function sendScrapedCarsToAPI(cars) {
+  const chunkSize = 10;
+  const carChunks = chunkArray(cars, chunkSize);
+
+  for (const chunk of carChunks) {
+    try {
+      const response = await axios.post(
+        "https://scraper-db-api.onrender.com/cars/new-cars",
+        {
+          cars: chunk,
+        }
+      );
+      console.log("Cars successfully added:", response.data);
+    } catch (error) {
+      console.error("Error adding cars:", error);
+    }
+  }
+}
 
 async function startCrawler() {
   const browser = await playwright.chromium.launch({
@@ -14,18 +137,22 @@ async function startCrawler() {
     `https://www.autotrader.ca/cars/ab/calgary/?rcp=15&rcs=0&srt=35&prx=100&prv=Alberta&loc=calgary&hprc=True&wcp=True&sts=New&showcpo=1&inMarket=advancedSearch`,
     {
       waitUntil: "domcontentloaded",
-      timeout: 60000,
+      timeout: 120000,
     }
   );
 
-  (await page.waitForSelector(".close-button")).click();
-  await page.waitForTimeout(10000);
+  const isCookiesPopUpVisible = await page.isVisible(".close-button");
+  if (isCookiesPopUpVisible) {
+    await page.click(".close-button");
+    await page.waitForTimeout(3000);
+  }
 
-  (await page.waitForSelector("select#pageSize")).click();
+  await page.waitForSelector("select#pageSize");
   await page.selectOption("select#pageSize", "100");
   await page.waitForTimeout(10000);
 
   let hasNextPage = true;
+  let carCounter = 0;
 
   while (hasNextPage) {
     // Handle pop-up if it appears
@@ -45,6 +172,8 @@ async function startCrawler() {
     console.log(`Found ${carLinks.length} car links`);
 
     for (const carLink of carLinks) {
+      carCounter++;
+
       await page.goto(carLink, {
         waitUntil: "domcontentloaded",
         timeout: 60000,
@@ -54,6 +183,8 @@ async function startCrawler() {
       try {
         const urlParts = carLink.split("/");
         const Make = urlParts[4] || "Not Available";
+        let Model = urlParts[5] || "Not Available";
+        Model = decodeURIComponent(Model);
 
         const carNameWithTrim = (await page.isVisible("h1.hero-title"))
           ? await page.locator("h1.hero-title").textContent()
@@ -77,14 +208,6 @@ async function startCrawler() {
         const match = carName.match(pattern);
         const Year = match ? match[0] : "No Year Found";
 
-        function getCarModel(carNameWithTrim, Trim) {
-          const carNameNoYear = carNameWithTrim.replace(/\b\d{4}\b/, "").trim();
-          const words = carNameNoYear.split(" ");
-          const model = words.slice(1, words.indexOf(Trim)).join(" ");
-          return model || "Model Not Found";
-        }
-        const Model = getCarModel(carNameWithTrim, Trim);
-
         const Location = "Calgary";
 
         const CoverImage =
@@ -102,9 +225,11 @@ async function startCrawler() {
           ? await page.locator("span#spec-value-1").textContent()
           : "Not Available";
 
-        const BodyType = (await page.isVisible("span#spec-value-3"))
+        let BodyType = (await page.isVisible("span#spec-value-3"))
           ? await page.locator("span#spec-value-3").textContent()
           : "Not Available";
+
+        BodyType = getMainBodyType(BodyType);
 
         const Engine = (await page.isVisible("span#spec-value-4"))
           ? await page.locator("span#spec-value-4").textContent()
@@ -165,15 +290,29 @@ async function startCrawler() {
           ? await page.locator("div#vdp-collapsible-short-text").textContent()
           : "Not Available";
 
+        //Dealer info will be available after database is updated
+        const Dealer = (await page.isVisible(".dealerInfo-name"))
+          ? await page.locator(".dealerInfo-name").textContent()
+          : "Not Available";
+
+        const Dealer_Phone = (await page.isVisible(".dealerInfo-phone"))
+          ? await page.locator(".dealerInfo-phone").textContent()
+          : "Not Available";
+
+        const carDealer = {
+          name: Dealer,
+          number: Dealer_Phone,
+        };
+
         const carDetails = {
           car_url: carLink,
           car_id: uuidv4(),
           Location,
-          Make,
-          Model,
+          Make: Make.toLowerCase(),
+          Model: Model.toLowerCase(),
           Trim,
           Mileage,
-          BodyType,
+          BodyType: BodyType.toLowerCase(),
           Year,
           Status,
           Price,
@@ -190,20 +329,21 @@ async function startCrawler() {
           Description,
         };
 
-        console.log(carDetails);
+        console.log(`Car_Number: #${carCounter}`);
+        cars.push(carDetails);
       } catch (error) {
         console.error(`Error scraping car at ${carLink}:`, error);
       }
       await page.waitForTimeout(5000);
     }
 
-    const nextPageSelector = "a.last-page-link";
-    const isNextPage = await page.$(nextPageSelector);
+    await sendScrapedCarsToAPI(cars);
 
-    if (isNextPage) {
+    const nextButton = await page.$("a.last-page-link");
+    if (nextButton) {
       console.log("Navigating to the next page...");
-      await page.click(nextPageSelector);
-      await page.waitForTimeout(30000);
+      await nextButton.click();
+      await page.waitForSelector(".dealer-split-wrapper");
     } else {
       console.log("No more pages to navigate.");
       hasNextPage = false;
@@ -215,173 +355,6 @@ async function startCrawler() {
 
 startCrawler();
 
-// requestHandler: async ({ request, page, log, enqueueLinks }) => {
-//     console.log(`Processing: ${request.url}`);
-//     if (request.label === "DETAIL") {
-//       //when in the car details page
-//       console.log(`Extracting data: ${request.url}`);
-
-//       const urlParts = request.url.split("/");
-//       const Make = urlParts[4] || "Not Available";
-
-//       const carNameWithTrim =
-//         (await page.locator("h1.hero-title").textContent()) ||
-//         "Not Available";
-
-//       const Trim =
-//         (await page.locator("span#spec-value-2").textContent()) ||
-//         "Not Available";
-
-//       function getCarName(carNameWithTrim, Trim) {
-//         const trimIndex = carNameWithTrim.indexOf(Trim);
-
-//         if (trimIndex !== -1) {
-//           return carNameWithTrim.substring(0, trimIndex).trim();
-//         } else {
-//           return carNameWithTrim;
-//         }
-//       }
-//       const carName = getCarName(carNameWithTrim, Trim);
-
-//       const pattern = /\b\d{4}\b/;
-//       const match = carName.match(pattern);
-//       const Year = match ? match[0] : "No Year Found";
-
-//       function getCarModel(carNameWithTrim, Trim) {
-//         const carNameNoYear = carNameWithTrim.replace(/\b\d{4}\b/, "").trim();
-
-//         const words = carNameNoYear.split(" ");
-
-//         const brand = words[0];
-//         const model = words.slice(1, words.indexOf(Trim)).join(" ");
-
-//         return model || "Model Not Found";
-//       }
-//       const Model = getCarModel(carNameWithTrim, Trim);
-
-//       const Location = "Calgary";
-
-//       const CoverImage =
-//         (await page.locator("img#mainPhoto.loaded").getAttribute("src")) ||
-//         "Not Available";
-
-//       await page.click("img#mainPhoto.loaded");
-//       await page.waitForSelector(".gallery-thumbnail img");
-//       const otherCarImages = await page.$$eval(
-//         ".gallery-thumbnail img",
-//         (imgs) => imgs.map((img) => img.src)
-//       );
-
-//       const Status =
-//         (await page.locator("span#spec-value-1").textContent()) ||
-//         "Not Available";
-
-//       const BodyType =
-//         (await page.locator("span#spec-value-3").textContent()) ||
-//         "Not Available";
-
-//       const Engine =
-//         (await page.locator("span#spec-value-4").textContent()) ||
-//         "Not Available";
-
-//       const Drivetrain =
-//         (await page.locator("span#spec-value-7").textContent()) ||
-//         "Not Available";
-
-//       const price =
-//         (await page.locator(".hero-price").textContent()) || "Not Available";
-//       const Price = `$${price}`;
-
-//       const Mileage =
-//         (await page.locator("span#spec-value-0").textContent()) ||
-//         "Not Available";
-
-//       const Doors =
-//         (await page.locator("span#spec-value-11").textContent()) ||
-//         "Not Available";
-
-//       const ExteriorColor =
-//         (await page.locator("span#spec-value-9").textContent()) ||
-//         "Not Available";
-
-//       const InteriorColor =
-//         (await page.locator("span#spec-value-10").textContent()) ||
-//         "Not Available";
-
-//       const FuelType =
-//         (await page.locator("span#spec-value-12").textContent()) ||
-//         "Not Available";
-
-//       const Transmission =
-//         (await page.locator("span#spec-value-6").textContent()) ||
-//         "Not Available";
-
-//       const Stock_Number =
-//         (await page.locator("span#spec-value-8").textContent()) ||
-//         "Not Available";
-
-//       const Description =
-//         (await page
-//           .locator("div#vdp-collapsible-short-text")
-//           .textContent()) || "Not Available";
-
-//       const carDetails = {
-//         car_url: request.url,
-//         car_id: uuidv4(),
-//         Location,
-//         Make,
-//         Model,
-//         Trim,
-//         Mileage,
-//         BodyType,
-//         Year,
-//         Status,
-//         Price,
-//         ExteriorColor,
-//         InteriorColor,
-//         Transmission,
-//         CoverImage,
-//         otherCarImages,
-//         Engine,
-//         Drivetrain,
-//         FuelType,
-//         Stock_Number,
-//         Doors,
-//         Description,
-//       };
-
-//       console.log(`Saving data: ${request.url}`);
-//       console.log(carDetails);
-//     } else {
-//       //when on the website's home page
-//       console.log(`Enqueueing car listings for: ${request.url}`);
-
-//       const productSelector = ".dealer-split-wrapper > a";
-//       await page.waitForSelector(productSelector);
-//       await enqueueLinks({
-//         selector: productSelector,
-//         label: "DETAIL",
-//       });
-
-//       const nextPageSelector = "a.last-page-link";
-//       const nextButton = await page.$(nextPageSelector);
-//       if (nextButton) {
-//         await enqueueLinks({
-//           selector: nextPageSelector,
-//         });
-//       }
-//     }
-//   },
-// });
-
-// await crawler.run([
-//   "https://www.autotrader.ca/cars/ab/calgary/?rcp=15&rcs=0&srt=35&prx=100&prv=Alberta&loc=calgary&hprc=True&wcp=True&sts=New&showcpo=1&inMarket=advancedSearch",
-// ]);
-// }
-
 // module.exports = {
 //   startCrawler,
 // };
-// startCrawler();
-
-// class for pop-up "ng-binding"
--- calgary/maserati.js
+++ calgary/maserati.js
@@ -1,133 +1,272 @@
 const playwright = require("playwright");
 const { v4: uuidv4 } = require("uuid");
-const dotenv = require("dotenv");
-dotenv.config();
+const axios = require("axios");
 
-async function startCrawler() {
-  const crawler = new PlaywrightCrawler({
-    launchContext: {
-      launchOptions: {
-        headless: true,
-      },
-    },
-    requestHandler: async ({ request, page, log, enqueueLinks }) => {
-      console.log(`Processing: ${request.url}`);
-      if (request.label === "DETAIL") {
-        //when in the car details page
-        console.log(`Extracting data: ${request.url}`);
+function getMainBodyType(bodyType) {
+  const bodyTypes = {
+    hatchback: [
+      "3-door hatchback",
+      "5-door hatchback",
+      "compact hatchback",
+      "subcompact hatchback",
+    ],
+    suv: [
+      "suv",
+      "crossover",
+      "compact suv",
+      "midsize suv",
+      "full-size suv",
+      "off-road suv",
+      "luxury suv",
+      "subcompact suv",
+    ],
+    coupe: [
+      "coupe",
+      "sports-utility",
+      "sports coupe",
+      "grand tourer",
+      "hardtop coupe",
+      "luxury coupe",
+      "performance coupe",
+    ],
+    truck: [
+      "truck",
+      "crew cab",
+      "pickup truck",
+      "pickup",
+      "light duty truck",
+      "heavy duty truck",
+      "extended cab",
+      "extended cab truck",
+      "compact truck",
+      "off-road truck",
+      "full-size truck",
+    ],
+    sedan: [
+      "compact sedan",
+      "subcompact sedan",
+      "midsize sedan",
+      "full-size sedan",
+      "executive sedan",
+      "luxury sedan",
+      "sports sedan",
+    ],
+    wagon: [
+      "wagon",
+      "super-wagon",
+      "station wagon",
+      "estate wagon",
+      "sports wagon",
+      "crossover wagon",
+      "luxury wagon",
+    ],
+    convertible: [
+      "convertible",
+      "roadster",
+      "cabriolet",
+      "hardtop convertible",
+      "soft-top convertible",
+      "targa",
+      "2-seater convertible",
+    ],
+    van: [
+      "van",
+      "minivan",
+      "passenger van",
+      "cargo van",
+      "full-size van",
+      "compact van",
+      "conversion van",
+      "camper van",
+    ],
+  };
 
-        const urlPath = new URL(request.url).pathname;
-        const carRegex = /\/vehicle\/(\d{4})-(\w+)-(\w+)-/;
-        const carMatch = urlPath.match(carRegex);
-        const Make = carMatch ? carMatch[2] : "Make Not Found";
-        const Model = carMatch ? carMatch[3] : "Model Not Found";
-        const Year = carMatch ? carMatch[1] : "Year Not Found";
+  try {
+    bodyType = bodyType.toLowerCase().replace(/[-\s]+/g, " ");
 
-        const Location = "Calgary";
-
-        const price =
-          (await page.locator("span#final-price").textContent()) ||
-          "Not Available";
-        const Price = `$${price}`;
-
-        const CoverImage =
-          (await page.locator("img[itemprop='image']").getAttribute("src")) ||
-          "Not Available";
-
-        await page.waitForSelector(".thumb img");
-        const otherCarImages = await page.$$eval(".thumb img", (imgs) =>
-          imgs.map((img) => img.src)
-        );
-
-        const BodyType =
-          (await page.locator("td[itemprop='bodyType']").textContent()) ||
-          "Not Available";
-
-        const Engine =
-          (await page.locator("td[itemprop='vehicleEngine']").textContent()) ||
-          "Not Available";
-
-        const Trim =
-          (await page.locator("[itemprop='model'] span").textContent()) ||
-          "Not Available";
-
-        const ExteriorColor =
-          (await page.locator("td[itemprop='color']").textContent()) ||
-          "Not Available";
-
-        const InteriorColor =
-          (await page
-            .locator("td[itemprop='vehicleInteriorColor']")
-            .textContent()) || "Not Available";
-
-        const Drivetrain =
-          (await page
-            .locator("div:nth-of-type(3) tr:nth-of-type(3) td.td-odd")
-            .textContent()) || "Not Available";
-
-        const FuelType =
-          (await page.locator("td[itemprop='fuelType']").textContent()) ||
-          "Not Available";
-
-        const Transmission =
-          (await page
-            .locator("td[itemprop='vehicleTransmission']")
-            .textContent()) || "Not Available";
-
-        const Stock_Number =
-          (await page.locator("td[itemprop='sku']").textContent()) ||
-          "Not Available";
-
-        const carDetails = {
-          car_url: request.url,
-          car_id: uuidv4(),
-          Location,
-          Make,
-          Model,
-          Trim,
-          BodyType,
-          Year,
-          Price,
-          ExteriorColor,
-          InteriorColor,
-          Transmission,
-          Drivetrain,
-          FuelType,
-          CoverImage,
-          otherCarImages,
-          Engine,
-          Stock_Number,
-        };
-
-        console.log(`Saving data: ${request.url}`);
-        console.log(carDetails);
-      } else if (request.label === "CATEGORY") {
-        //when in the car listing page
-        console.log(`Enqueueing pagination for: ${request.url}`);
-
-        const carSelector = ".item.active > a";
-        await page.waitForSelector(carSelector);
-        await enqueueLinks({
-          selector: carSelector,
-          label: "DETAIL",
-        });
-      } else {
-        //when on the website's home page
-        console.log(`Enqueueing categories from page: ${request.url}`);
-
-        const newCarSelector = ".menu-item-2308 > a";
-        await page.waitForSelector(newCarSelector);
-        await enqueueLinks({
-          selector: newCarSelector,
-          label: "CATEGORY",
-        });
+    for (let mainType in bodyTypes) {
+      if (mainType === bodyType || bodyTypes[mainType].includes(bodyType)) {
+        return mainType;
       }
-    },
-  });
+    }
 
-  await crawler.run(["https://www.maseratiofalberta.com/"]);
+    throw new Error("Body type not found");
+  } catch (error) {
+    return "Not Available";
+  }
 }
 
-module.exports = {
-  startCrawler,
-};
+const cars = [];
+
+function chunkArray(array, size) {
+  const result = [];
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+  return result;
+}
+
+async function sendScrapedCarsToAPI(cars) {
+  const chunkSize = 10;
+  const carChunks = chunkArray(cars, chunkSize);
+
+  for (const chunk of carChunks) {
+    try {
+      const response = await axios.post(
+        "https://scraper-db-api.onrender.com/cars/new-cars",
+        {
+          cars: chunk,
+        }
+      );
+      console.log("Cars successfully added:", response.data);
+    } catch (error) {
+      console.error("Error adding cars:", error);
+    }
+  }
+}
+
+async function startCrawler() {
+  const browser = await playwright.chromium.launch({
+    headless: false,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(`https://www.maseratiofalberta.com/`, {
+    waitUntil: "domcontentloaded",
+    timeout: 120000,
+  });
+
+  let carCounter = 0;
+
+  const newCarSelector = await page.isVisible(".menu-item-2308 > a");
+  if (newCarSelector) {
+    console.log("New car link selected...");
+    await page.click(".menu-item-2308 > a");
+    await page.waitForTimeout(10000);
+  }
+
+  const productSelector = ".item.active > a";
+  await page.waitForTimeout(10000);
+  const carLinks = await page.$$eval(productSelector, (links) =>
+    links.map((link) => link.href)
+  );
+
+  console.log(`Found ${carLinks.length} car links`);
+  for (const carLink of carLinks) {
+    carCounter++;
+
+    await page.goto(carLink, {
+      waitUntil: "domcontentloaded",
+      timeout: 60000,
+    });
+
+    try {
+      const urlPath = new URL(carLink).pathname;
+      const carRegex = /\/vehicle\/(\d{4})-(\w+)-(\w+)-/;
+      const carMatch = urlPath.match(carRegex);
+      const Make = carMatch ? carMatch[2] : "Make Not Found";
+      const Model = carMatch ? carMatch[3] : "Model Not Found";
+      const Year = carMatch ? carMatch[1] : "Year Not Found";
+
+      const Location = "Calgary";
+
+      const price = (await page.isVisible("span#final-price"))
+        ? await page.locator("span#final-price").textContent()
+        : "Not Available";
+      const Price = `$${price}`;
+
+      const CoverImage =
+        (await page.locator("img[itemprop='image']").getAttribute("src")) ||
+        "Not Available";
+
+      await page.waitForSelector(".thumb img");
+      const otherCarImages = await page.$$eval(".thumb img", (imgs) =>
+        imgs.map((img) => img.src)
+      );
+
+      const BodyType = (await page.isVisible("td[itemprop='bodyType']"))
+        ? await page.locator("td[itemprop='bodyType']").textContent()
+        : "Not Available";
+
+      const Engine = (await page.isVisible("td[itemprop='vehicleEngine']"))
+        ? await page.locator("td[itemprop='vehicleEngine']").textContent()
+        : "Not Available";
+
+      const Trim = (await page.isVisible("[itemprop='model'] span"))
+        ? await page.locator("[itemprop='model'] span").textContent()
+        : "Not Available";
+
+      const ExteriorColor = (await page.locator("td[itemprop='color']"))
+        ? await page.locator("td[itemprop='color']").textContent()
+        : "Not Available";
+
+      const InteriorColor = (await page.isVisible(
+        "td[itemprop='vehicleInteriorColor']"
+      ))
+        ? await page
+            .locator("td[itemprop='vehicleInteriorColor']")
+            .textContent()
+        : "Not Available";
+
+      const Drivetrain = (await page.isVisible(
+        "div:nth-of-type(3) tr:nth-of-type(3) td.td-odd"
+      ))
+        ? await page
+            .locator("div:nth-of-type(3) tr:nth-of-type(3) td.td-odd")
+            .textContent()
+        : "Not Available";
+
+      const FuelType = (await page.isVisible("td[itemprop='fuelType']"))
+        ? await page.locator("td[itemprop='fuelType']").textContent()
+        : "Not Available";
+
+      const Transmission = (await page.isVisible(
+        "td[itemprop='vehicleTransmission']"
+      ))
+        ? await page.locator("td[itemprop='vehicleTransmission']").textContent()
+        : "Not Available";
+
+      const Stock_Number = (await page.isVisible("td[itemprop='sku']"))
+        ? await page.locator("td[itemprop='sku']").textContent()
+        : "Not Available";
+
+      const carDetails = {
+        car_url: carLink,
+        car_id: uuidv4(),
+        Location,
+        Make: Make.toLowerCase(),
+        Model: Model.toLowerCase(),
+        Trim,
+        BodyType: BodyType.toLowerCase(),
+        Year,
+        Price,
+        ExteriorColor,
+        InteriorColor,
+        Transmission,
+        Drivetrain,
+        FuelType,
+        CoverImage,
+        otherCarImages,
+        Engine,
+        Stock_Number,
+      };
+
+      console.log(`Car_Number: #${carCounter}`);
+      cars.push(carDetails);
+    } catch (error) {
+      console.error(`Error scraping car at ${carLink}:`, error);
+    }
+
+    await page.waitForTimeout(5000);
+  }
+  await sendScrapedCarsToAPI(cars);
+
+  await browser.close();
+}
+
+startCrawler();
+
+// module.exports = {
+//   startCrawler,
+// };
